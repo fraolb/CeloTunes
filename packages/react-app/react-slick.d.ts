a4c67b4e90d8d58dfb62c804846b6a5675ae91cf
fraolb <Fraolbereket@gmail.com>
2024-05-29 12:45:13 +0300

    added react-slick config

---
+++ packages/react-app/react-slick.d.ts
@@ -0,0 +1,57 @@
+declare module "react-slick" {
+  import * as React from "react";
+
+  interface Settings {
+    accessibility?: boolean;
+    adaptiveHeight?: boolean;
+    afterChange?: (currentSlide: number) => void;
+    appendDots?: (dots: React.ReactNode) => React.ReactNode;
+    arrows?: boolean;
+    asNavFor?: string;
+    autoplay?: boolean;
+    autoplaySpeed?: number;
+    beforeChange?: (currentSlide: number, nextSlide: number) => void;
+    centerMode?: boolean;
+    centerPadding?: string;
+    className?: string;
+    cssEase?: string;
+    customPaging?: (index: number) => React.ReactNode;
+    dots?: boolean;
+    dotsClass?: string;
+    draggable?: boolean;
+    easing?: string;
+    edgeFriction?: number;
+    fade?: boolean;
+    focusOnSelect?: boolean;
+    infinite?: boolean;
+    initialSlide?: number;
+    lazyLoad?: "ondemand" | "progressive";
+    pauseOnDotsHover?: boolean;
+    pauseOnFocus?: boolean;
+    pauseOnHover?: boolean;
+    responsive?: Array<{ breakpoint: number; settings: Partial<Settings> }>;
+    rows?: number;
+    rtl?: boolean;
+    slide?: string;
+    slidesPerRow?: number;
+    slidesToScroll?: number;
+    slidesToShow?: number;
+    speed?: number;
+    swipe?: boolean;
+    swipeEvent?: (swipeDirection: "left" | "right" | "up" | "down") => void;
+    swipeToSlide?: boolean;
+    touchMove?: boolean;
+    touchThreshold?: number;
+    useCSS?: boolean;
+    useTransform?: boolean;
+    variableWidth?: boolean;
+    vertical?: boolean;
+    verticalSwiping?: boolean;
+    waitForAnimate?: boolean;
+    [key: string]: any;
+  }
+
+  class Slider extends React.Component<Settings> {}
+
+  export default Slider;
+}
